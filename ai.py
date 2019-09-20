f8176aee8c99bbd66c731f8cb14be4e464b9c3d2
bonkonst <Bondarenko@MacBook-Pro-Konstantin.local>
2019-09-20 17:07:52 +0300

    one more initial

--- ai.py
+++ ai.py
@@ -1,18 +1,3 @@
-my_name = "Konstantin"
-current_day = "September 20 2019"
-print("Good day" + ' ' + my_name + "!" + current_day + ' ' + "is a perfect day to learn some python.")
-
-first_name = "Bondarenko"
-last_name = "Konstantin"
-print(first_name + ' ' + last_name)
-
-first_variable = 25
-second_variable = 30
-print(first_variable + second_variable,
-      first_variable - second_variable,
-      first_variable / second_variable,
-      first_variable * second_variable,
-      first_variable // second_variable,
-      first_variable % second_variable,
-      first_variable ** second_variable
-      )
+# number1 = int(input('enter first number: '))
+# umber2 = int(input('enter second number: '))
+# print((number1+number2))
